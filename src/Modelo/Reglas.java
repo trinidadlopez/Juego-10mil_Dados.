faa38a464a0c81c12a51d57c916f8ff114e479a1
trinidadlopez <lopeztrini666@gmail.com>
2025-07-23 19:25:01 -0300

    add Regla

---
+++ src/Modelo/Reglas.java
@@ -0,0 +1,58 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+public class Reglas {
+    ArrayList<Dado> dadosApartados;
+
+
+    public int[] cantidadDados(ArrayList<Dado> dados){ //cuenta la cantidad de dados que hay de cada numero
+        int[] conteo = new int[7];
+        for(Dado d : dados){
+            int valorDado = d.getValorCaraSuperior();
+            conteo[valorDado]++;
+        }
+        return conteo;
+    }
+    protected boolean tieneTrio(int[] conteo){ //chequeo si tiene trio de alguno de los numeros
+        for(int i=1; i <= 6; i++ ){
+            if(conteo[i]>=3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected boolean tieneEscalera(int[] conteo){ //chequeo si tiene escalera
+        boolean escalera1a5 = true; //chequeo escalera: 1,2,3,4,5
+        for(int i=1; i <= 5; i++){
+            if(conteo[i] != 1){
+                escalera1a5=false;
+            }
+        }
+        if(escalera1a5){
+            return true;
+        }
+
+        boolean escalera2a6 = true; //chequeo escalera: 2,3,4,5,6
+        for(int i=2; i <= 6; i++){
+            if(conteo[i] != 1){
+                escalera2a6=false;
+            }
+        }
+        if(escalera2a6){
+            return true;
+        }
+
+        if(conteo[1]==1 && conteo[3]==1 && conteo[4]==1 && conteo[5]==1 && conteo[6]==1){ //chequeo 3,4,5,6,1
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public void actualizar_dados_apartados(ArrayList<Dado> new_dados_apartados){
+        dadosApartados = new_dados_apartados;
+    }
+}
